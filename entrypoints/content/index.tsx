d4269b66f8c6218ed83e63e9b4204324f636bfd7
Tomo Wang <tomo_wang@163.com>
2026-01-13 22:42:50 +0800

    refactor: dynamic event binding on config change

--- entrypoints/content/index.tsx
+++ entrypoints/content/index.tsx
@@ -25,66 +25,65 @@ export default defineContentScript({
     const url = new URL(location.href);
     const params = new URLSearchParams(url.search);
 
-    const openMode = await storage.getItem<number>(OPEN_MODE_STORAGE_KEY, {
-      fallback: OpenMode.BOTH,
-    });
-    const closeMode = await storage.getItem<number>(CLOSE_MODE_STORAGE_KEY, {
-      fallback: CloseMode.BOTH,
-    });
-    const triggerKey = await storage.getItem<string>(TRIGGER_KEY_STORAGE_KEY, {
-      fallback: TriggerKey.SHIFT,
-    });
-
     let ui: null | ShadowRootContentScriptUi<{
       root: ReactDOM.Root;
       wrapper: HTMLDivElement;
     }> = null;
-    window.addEventListener("message", async (event: MessageEvent) => {
-      log("message received: ", event.data);
-      if (
-        event.data.channel === MESSAGE_CHANNEL &&
-        event.data.action === MessageActions.CLOSE_IFRAME_POPUP
-      ) {
-        await browser.runtime.sendMessage<IFrameMessage>({
-          action: MessageActions.CLOSE_IFRAME_POPUP,
-          url: event.data.url,
-        });
-        ui?.remove();
-      }
-    });
 
-    // peek preview window
-    if (params.has(EXT_PARAM_KEY)) {
-      // escape to close
-      if (closeMode && closeMode & CloseMode.ESCAPE) {
-        ctx.addEventListener(
-          window,
-          "keydown",
-          async function close(event: KeyboardEvent) {
+    // cleanup events
+    let cleanup = () => {};
+
+    async function mountEvents() {
+      // remove old events
+      cleanup();
+
+      const unwatchList: Array<() => void> = [];
+      cleanup = () => {
+        unwatchList.forEach((u) => u());
+      };
+
+      const openMode = await storage.getItem<number>(OPEN_MODE_STORAGE_KEY, {
+        fallback: OpenMode.BOTH,
+      });
+      const closeMode = await storage.getItem<number>(CLOSE_MODE_STORAGE_KEY, {
+        fallback: CloseMode.BOTH,
+      });
+      const triggerKey = await storage.getItem<string>(
+        TRIGGER_KEY_STORAGE_KEY,
+        {
+          fallback: TriggerKey.SHIFT,
+        }
+      );
+
+      // peek preview window
+      if (params.has(EXT_PARAM_KEY)) {
+        // escape to close
+        if (closeMode && closeMode & CloseMode.ESCAPE) {
+          const close = async function (event: KeyboardEvent) {
             if (event.key === "Escape") {
               await browser.runtime.sendMessage<CloseMessage>({
                 action: MessageActions.CLOSE,
               });
             }
-          }
-        );
-      }
+          };
+          window.addEventListener("keydown", close);
+          unwatchList.push(() => window.removeEventListener("keydown", close));
+        }
 
-      // blur to close
-      if (closeMode && closeMode & CloseMode.BLUR) {
-        ctx.addEventListener(window, "blur", async function close() {
-          await browser.runtime.sendMessage<CloseMessage>({
-            action: MessageActions.CLOSE,
-          });
-        });
-      }
-    } else {
-      // open links with shortcut key
-      if (openMode && openMode & OpenMode.SHORTCUT_CLICK) {
-        ctx.addEventListener(
-          document,
-          "click",
-          async (event: MouseEvent) => {
+        // blur to close
+        if (closeMode && closeMode & CloseMode.BLUR) {
+          const close = async function () {
+            await browser.runtime.sendMessage<CloseMessage>({
+              action: MessageActions.CLOSE,
+            });
+          };
+          window.addEventListener("blur", close);
+          unwatchList.push(() => window.removeEventListener("blur", close));
+        }
+      } else {
+        // open links with shortcut key
+        if (openMode && openMode & OpenMode.SHORTCUT_CLICK) {
+          const onClick = async (event: MouseEvent) => {
             let triggered = false;
             switch (triggerKey) {
               case TriggerKey.SHIFT:
@@ -105,22 +104,52 @@ export default defineContentScript({
             }
             if (!triggered) return;
             ui = await openPopup(event, ctx);
-          },
-          { capture: true }
-        );
-      }
-      // open links with drag
-      if (openMode && openMode & OpenMode.DRAG_LINK) {
-        ctx.addEventListener(
-          document,
-          "dragend",
-          async (event: DragEvent) => {
+          };
+          document.addEventListener("click", onClick, { capture: true });
+          unwatchList.push(() =>
+            document.removeEventListener("click", onClick, { capture: true })
+          );
+        }
+        // open links with drag
+        if (openMode && openMode & OpenMode.DRAG_LINK) {
+          const onDragEnd = async (event: DragEvent) => {
             ui = await openPopup(event, ctx);
-          },
-          { capture: true }
-        );
+          };
+          document.addEventListener("dragend", onDragEnd, { capture: true });
+          unwatchList.push(() =>
+            document.removeEventListener("dragend", onDragEnd, {
+              capture: true,
+            })
+          );
+        }
       }
     }
+
+    await mountEvents();
+
+    storage.watch<any>(OPEN_MODE_STORAGE_KEY, async () => {
+      await mountEvents();
+    });
+    storage.watch<any>(CLOSE_MODE_STORAGE_KEY, async () => {
+      await mountEvents();
+    });
+    storage.watch<any>(TRIGGER_KEY_STORAGE_KEY, async () => {
+      await mountEvents();
+    });
+
+    window.addEventListener("message", async (event: MessageEvent) => {
+      log("message received: ", event.data);
+      if (
+        event.data.channel === MESSAGE_CHANNEL &&
+        event.data.action === MessageActions.CLOSE_IFRAME_POPUP
+      ) {
+        await browser.runtime.sendMessage<IFrameMessage>({
+          action: MessageActions.CLOSE_IFRAME_POPUP,
+          url: event.data.url,
+        });
+        ui?.remove();
+      }
+    });
   },
 });
 
